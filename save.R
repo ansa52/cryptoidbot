4e637955449adc36ad6a2c78fa885a2c4461f15b
ansa52 <100661892+ansa52@users.noreply.github.com>
2022-06-25 16:18:03 +0700

    Create save.R

---
+++ save.R
@@ -0,0 +1,39 @@
+library(rvest)
+library(jsonlite)
+library(tidyverse)
+library(mongolite)
+
+
+#mendapatkan list crypto
+#currencySymbol, name, logo
+url <- Sys.getenv("CRYPTO_URL_1")
+crypto <- fromJSON(url)
+crypto <- crypto$payload
+
+symbol <- crypto['currencySymbol']
+for(i in 1:67){
+  symbol[i,] <- paste0(tolower(symbol[i,]),"/idr")
+}
+crypto['pair'] <- symbol
+
+#mendapatkan update harga
+url2 <- Sys.getenv("CRYPTO_URL_2")
+update <- fromJSON(url2)
+update <-update$payload
+
+#menggabungkan crypto dan update
+data_crypto <- merge(crypto,update,by="pair")
+
+data_json <- data_crypto %>% select(pair,currencyGroup,name,logo,latestPrice,day,week,month,year)
+
+
+#koneksi ke mongodb
+cs = Sys.getenv("CRYPTO_MONGO_CON")
+harga_crypto_collection = mongo(collection=Sys.getenv("CRYPTO_MONGO_COL"), db=Sys.getenv("CRYPTO_MONGO_DB"), url=cs)
+
+#insert data_json
+if(harga_crypto_collection$count() > 0){
+  harga_crypto_collection$remove('{}')
+} else {
+  harga_crypto_collection$insert(data_json)
+}
