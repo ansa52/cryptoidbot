c7bea11629111f27f8724ebef47554f13233583d
ansa52 <100661892+ansa52@users.noreply.github.com>
2022-06-26 00:17:49 +0700

    Update publish.R

--- publish.R
+++ publish.R
@@ -1,6 +1,10 @@
+#library
 library(rtweet)
 library(tidyverse)
 library(mongolite)
+library(gt)
+library(gtExtras)
+library(webshot2)
 
 
 #koneksi ke mongodb
@@ -9,21 +13,37 @@ harga_crypto_collection = mongo(collection=Sys.getenv("CRYPTO_MONGO_COL"), db=Sy
 
 #insert data_json
 crypto <- harga_crypto_collection$find('{}')
-glimpse(crypto)
-high_price <- crypto[order(crypto$latestPrice, decreasing = TRUE),]
-high <- high_price[1:5,3:7]
 
+# 5 crypto dengan harga tertinggi
+high_price <- crypto[order(crypto$latestPrice, decreasing = TRUE),]
+high <- high_price[1:5,3:9]
+
+# 5 crypto dengan harga terendah
 low_price <- crypto[order(crypto$latestPrice, decreasing = FALSE),]
-low <- low_price[1:5,3:7]
+low <- low_price[1:5,3:9]
 
 
 tabel <- high %>%
   mutate(logo = map(logo, gt::html)) %>% gt() %>%
-  tab_spanner(label = "Perubahan Harga", columns = matches("day|week|month|year")) %>%
   tab_header(title = md("5 Crypto Harga Tertinggi")) %>% 
+  tab_spanner(label = "Perubahan Harga", columns = matches("day|week|month|year")) %>%
   fmt_currency(columns = latestPrice, currency = "IDR", sep_mark = ".", dec_mark = ",") %>%
   gt_img_rows(columns = logo, img_source = "web", height = 30) %>%
   tab_options(data_row.padding = px(1))
+
+tabel2 <- low %>%
+  mutate(logo = map(logo, gt::html)) %>% gt() %>%
+  tab_header(title = md("5 Crypto Harga Terendah")) %>% 
+  tab_spanner(label = "Perubahan Harga", columns = matches("day|week|month|year")) %>%
+  fmt_currency(columns = latestPrice, currency = "IDR", sep_mark = ".", dec_mark = ",") %>%
+  gt_img_rows(columns = logo, img_source = "web", height = 30) %>%
+  tab_options(data_row.padding = px(1))
+
+tmp <- tempfile(fileext = '.png') #path to temp .png file
+gtsave_extra(tabel,tmp) #save gt table as png
+
+tmp2 <- tempfile(fileext = '.png') #path to temp .png file
+gtsave_extra(tabel,tmp2) #save gt table as png
 
 
 ## Create Twitter token
@@ -35,20 +55,20 @@ crypto_token <- rtweet::create_token(
   access_secret =   Sys.getenv("CRYPTO_TWITTER_ACCESS_TOKEN_SECRET")
 )
 
-#data <- high[1,]
-
-install.packages("gt")
-library(gt)
-
 status_details <- paste0(
-  "Name: ", data$name, "\n",
-  "Harga(Rp): ", data$latestPrice, " \n"
+  "Update Harga Crypto", data$name, "\n\n"
 )
-
 
 ## Post the image to Twitter
 rtweet::post_tweet(
   status = status_details,
+  media = tmp,
+  token = crypto_token
+)
+
+rtweet::post_tweet(
+  status = status_details,
+  media = tmp2,
   token = crypto_token
 )
 
