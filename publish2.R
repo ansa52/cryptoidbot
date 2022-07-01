c5c116898a63c3f5150d04d020dd40178c01a14e
ansa52 <100661892+ansa52@users.noreply.github.com>
2022-07-01 07:26:56 +0700

    Create publish2.R

---
+++ publish2.R
@@ -0,0 +1,67 @@
+#library
+library(rtweet)
+library(tidyverse)
+library(mongolite)
+library(gt)
+library(gtExtras)
+library(webshot2)
+
+
+#koneksi ke mongodb
+cs = Sys.getenv("CRYPTO_MONGO_CON")
+harga_crypto_collection = mongo(collection=Sys.getenv("CRYPTO_MONGO_COL"), db=Sys.getenv("CRYPTO_MONGO_DB"), url=cs)
+
+#get all data from mongodb
+crypto <- harga_crypto_collection$find('{}')
+
+crypto <- crypto %>% rename(
+  Crypto = name,
+  Logo = logo,
+  Harga = latestPrice,
+  "24jam" = day,
+  "1mgg" = week,
+  "1bln" = month,
+  "1thn" = year
+)
+
+
+# data diurutkan dari harga tertinggi
+high_price <- crypto[order(crypto$Harga, decreasing = TRUE),]
+high <- high_price[,3:9]
+
+
+tabel <- high %>%
+  mutate(Logo = map(Logo, gt::html)) %>% gt() %>%
+  tab_header(title = md("Harga Crypto Terkini")) %>% 
+  tab_spanner(label = "Perubahan Harga (%)", columns = matches("24jam|1mgg|1bln|1thn")) %>%
+  fmt_currency(columns = Harga, currency = "IDR", sep_mark = ".", dec_mark = ",") %>%
+  gt_img_rows(columns = Logo, img_source = "web", height = 30) %>%
+  tab_options(data_row.padding = px(1))
+
+
+
+tmp <- tempfile(fileext = '.png') #path to temp .png file
+gtsave_extra(tabel,tmp) #save gt table as png
+
+
+
+## Create Twitter token
+crypto_token <- rtweet::create_token(
+  app = Sys.getenv("CRYPTO_APP"), 
+  consumer_key =    Sys.getenv("CRYPTO_TWITTER_CONSUMER_API_KEY"),
+  consumer_secret = Sys.getenv("CRYPTO_TWITTER_CONSUMER_API_SECRET"),
+  access_token =    Sys.getenv("CRYPTO_TWITTER_ACCESS_TOKEN"),
+  access_secret =   Sys.getenv("CRYPTO_TWITTER_ACCESS_TOKEN_SECRET")
+)
+
+status_details <- paste0(
+  "Update Harga Crypto\n\n"
+)
+
+## Post the image to Twitter
+rtweet::post_tweet(
+  status = status_details,
+  media = tmp,
+  token = crypto_token
+)
+
